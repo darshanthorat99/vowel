9dca65e345654ba50225d496e3c1a6b80ceea8e7
darshanthorat99 <113414840+darshanthorat99@users.noreply.github.com>
2022-09-20 12:11:57 +0530

    Add project files.

---
+++ vowel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace vowel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the alphabet");
+            Char ch = Convert.ToChar(Console.ReadLine());
+                if(ch == 'a'|| ch == 'e'||ch =='i'|| ch =='o'||ch =='u')
+            {
+                Console.WriteLine($"{ch} is vowel");
+            }
+            else
+            {
+                Console.WriteLine("not a vowel");
+            }
+        }
+    }
+}
